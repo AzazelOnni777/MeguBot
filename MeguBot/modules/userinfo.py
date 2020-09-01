9970fbb7ccc15a354636da7f3569ce0a9bd98c32
NachABR <67166086+NachABR@users.noreply.github.com>
2020-09-01 15:27:50 -0300

    Update userinfo.py

--- MeguBot/modules/userinfo.py
+++ MeguBot/modules/userinfo.py
@@ -30,10 +30,10 @@ def about_me(update: Update, context: CallbackContext):
     elif message.reply_to_message:
         username = message.reply_to_message.from_user.first_name
         update.effective_message.reply_text(
-            f"{username} hasn't set an info message about themselves yet!")
+            f"{username} aún no ha establecido un mensaje de información sobre el!")
     else:
         update.effective_message.reply_text(
-            "You haven't set an info message about yourself yet!")
+            "Aún no estableciste un mensaje de información sobre ti!")
 
 
 @run_async
@@ -55,12 +55,12 @@ def set_about_me(update: Update, context: CallbackContext):
         if len(info[1]) < MAX_MESSAGE_LENGTH // 4:
             sql.set_user_me_info(user_id, info[1])
             if user_id == bot.id:
-                message.reply_text("Updated my info!")
+                message.reply_text("Actualicé mi información!")
             else:
-                message.reply_text("Updated your info!")
+                message.reply_text("Actualicé tu información!")
         else:
             message.reply_text(
-                "The info needs to be under {} characters! You have {}.".format(
+                "La información debe tener menos de {} caracteres! Tu tienes {}.".format(
                     MAX_MESSAGE_LENGTH // 4, len(info[1])))
 
 
@@ -84,10 +84,10 @@ def about_bio(update: Update, context: CallbackContext):
     elif message.reply_to_message:
         username = user.first_name
         update.effective_message.reply_text(
-            f"{username} hasn't had a message set about themselves yet!")
+            f"{username} aún no ha establecido un mensaje sobre sí mismo!")
     else:
         update.effective_message.reply_text(
-            "You haven't had a bio set about yourself yet!")
+            "Aún no tienes una biografía sobre ti!")
 
 
 @run_async
@@ -102,13 +102,13 @@ def set_about_bio(update: Update, context: CallbackContext):
 
         if user_id == message.from_user.id:
             message.reply_text(
-                "Ha, you can't set your own bio! You're at the mercy of others here..."
+                "No puedes establecer tu propia biografía! Estás a merced de los demás aquí..."
             )
             return
 
         if user_id == bot.id and sender_id not in SUDO_USERS and sender_id not in DEV_USERS:
             message.reply_text(
-                "Erm... yeah, I only trust sudo users or developers to set my bio."
+                "Erm..., solo confío en los usuarios sudo y desarrolladores para configurar mi biografía."
             )
             return
 
@@ -124,30 +124,30 @@ def set_about_bio(update: Update, context: CallbackContext):
                     repl_message.from_user.first_name))
             else:
                 message.reply_text(
-                    "A bio needs to be under {} characters! You tried to set {}."
+                    "Una biografía debe tener menos de {} caracteres! Intentaste configurar {}."
                     .format(MAX_MESSAGE_LENGTH // 4, len(bio[1])))
     else:
-        message.reply_text("Reply to someone's message to set their bio!")
+        message.reply_text("Responde al mensaje de alguien para establecer su biografía!")
 
 
 def __user_info__(user_id):
     bio = html.escape(sql.get_user_bio(user_id) or "")
     me = html.escape(sql.get_user_me_info(user_id) or "")
     if bio and me:
-        return f"<b>About user:</b>\n{me}\n<b>What others say:</b>\n{bio}"
+        return f"<b>Acerca del usuario:</b>\n{me}\n<b>Lo que otros dicen:</b>\n{bio}"
     elif bio:
-        return f"<b>What others say:</b>\n{bio}\n"
+        return f"<b>Lo que otros dicen:</b>\n{bio}\n"
     elif me:
-        return f"<b>About user:</b>\n{me}"
+        return f"<b>Acerca del usuario:</b>\n{me}"
     else:
         return ""
 
 
 __help__ = """
- • `/setbio <text>`*:* while replying, will save another user's bio
- • `/bio`*:* will get your or another user's bio. This cannot be set by yourself.
- • `/setme <text>`*:* will set your info
- • `/me`*:* will get your or another user's info
+ • `/setbio <text>`*:* Mientras responde, guardará la biografía de otro usuario.
+ • `/bio`*:* Obtendrá tu biografía o la de otro usuario. Esto no lo puede configurar usted mismo.
+ • `/setme <text>`*:* Establecerá su información.
+ •` /me`*:* Obtendrá tu información o la de otro usuario.
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
@@ -161,7 +161,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Bios and Abouts"
+__mod_name__ = "Bios e info"
 __command_list__ = ["setbio", "bio", "setme", "me"]
 __handlers__ = [
     SET_BIO_HANDLER, GET_BIO_HANDLER, SET_ABOUT_HANDLER, GET_ABOUT_HANDLER
