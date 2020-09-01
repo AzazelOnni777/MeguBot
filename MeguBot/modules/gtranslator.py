b73c098816b845dc59828508e049d59cfe547431
NachABR <67166086+NachABR@users.noreply.github.com>
2020-08-31 23:56:08 -0300

    Update gtranslator.py

--- MeguBot/modules/gtranslator.py
+++ MeguBot/modules/gtranslator.py
@@ -111,9 +111,9 @@ def totranslate(update: Update, context: CallbackContext):
     except IndexError:
         update.effective_message.reply_text(
             "Responder mensajes o escribir mensajes en otros idiomas para traducirlos al idioma deseado\n\n"
-            "Ejemplo: `/tr en-ml` para traducir del inglés al Malayalam\n"
-            "O use: `/tr ml` para la detección automática y su traducción al malayalam.\n"
-            "*Lista de códigos de idioma:*\naf,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,vi,xh,yi,yo,zh,zh_CN,zh_TW,zu",
+            "*Ejemplo:* `/tr en-ml` Para traducir del inglés al Malayalam\n"
+            "*O use:* `/tr ml` Para la detección automática y su traducción al malayalam.\n\n"
+            "*Lista de códigos de idioma:*\n\n`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`",
             parse_mode="markdown",
             disable_web_page_preview=True)
     except ValueError:
@@ -125,8 +125,8 @@ def totranslate(update: Update, context: CallbackContext):
 
 __help__ = """
 • `/tr` o `/tl` (código de idioma) como respuesta a un mensaje largo.
-* Ejemplo: * `/ tr es` *:* Traduce algo al español.
-          `/ tr en-es` *:* Traduce del inglés al español.
+*Ejemplo:* `/ tr es`*:* Traduce algo al español.
+           `/ tr en-es`*:* Traduce del inglés al español.
 """
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
